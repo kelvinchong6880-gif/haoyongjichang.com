17acbf0149546ac1f48c026ef81749f26ec3f16e
haoyongjichang <admin@haoyongjichang.com>
2026-09-02 16:35:55 +0800

    Clean up default Astro template constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '好用机场';
+export const SITE_DESCRIPTION = '好用机场(JiChangDog)为您提供全网最客观的高性价比与高端专线机场评测，以及最新的科学上网客户端教程与防跑路选购指南。';
